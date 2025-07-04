5fc69c7d6c2c2e068b29930bb7a8733af400c811
tklepacki <t.klepacki@thesignalgroup.com>
2025-07-05 00:36:19 +0200

    Adding fix

---
+++ cypress/tests/tonnageList.visual.ts
@@ -0,0 +1,9 @@
+import * as common from "../pages/common/common";
+
+describe('Tonnage List - Visual Tests', () => {
+    it('Login Page', () => {
+        cy.visit('/');
+        // common.inactivePace().should('exist')
+        ///cy.matchImageSnapshot();
+    });
+});
