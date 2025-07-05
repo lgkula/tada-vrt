9748dcf37d65fb6da03cde2812a83c03d280ab31
tklepacki <t.klepacki@thesignalgroup.com>
2025-07-06 00:41:28 +0200

    Adding fix

---
+++ cypress/tests/tonnageList-mock.visual.ts
@@ -0,0 +1,35 @@
+import * as common from "../pages/common/common";
+
+describe('Tonnage List - Visual Tests', () => {
+    it('Login Page', () => {
+        cy.intercept('GET', '**/api/tonnageList?basisPortID**', {
+            fixture: 'tonnageList.json'
+        }).as('getTonnageList');
+
+        cy.intercept('POST', '**/api/tonnageList/tce?tabId**', {
+            fixture: 'tce.json'
+        }).as('getTCE');
+
+        cy.visit('/');
+        common.inactivePace().should('exist')
+        cy.get('#enterEmailFormEmail', { timeout: 10000 }).should('be.visible').type('user1@stresstest.com', { delay: 100 });
+        cy.get('#enterEmailFormSubmit').should('be.visible').click();
+        cy.get('#password', { timeout: 10000 }).should('be.visible').type('Qweasd12@', { delay: 100 });
+        cy.get('#submitLogin').should('be.visible').click();
+        cy.contains('button', 'Accept').click();
+        cy.get("div[class^='SideNav_headerTitle_']", { timeout: 30000 }).should('be.visible');
+        cy.get("#sideNav-Tab--Tonnage-List").click()
+        common.inactivePace().should('exist')
+        cy.wait('@getTonnageList');
+        cy.wait('@getTCE');
+        cy.waitForDomToBeReady();
+        cy.matchImageSnapshot({
+            failureThreshold: 0.01,
+            failureThresholdType: 'percent',
+            blackout: [
+                '.clock',
+                '[class^="UserMenu_sideNavHeaderSettings_"]'
+            ],
+        });
+    });
+});
